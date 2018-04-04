3e1acf5f7fa43752c77aa520cc975f12034b9b47
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-04-04 00:13:23 -0300

    Contato Component About Component Home (content) Component

---
+++ src/Contato.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contato = props => {
+    return (
+        <div className='card'>
+            <h2>Fale Conosco</h2>
+        </div>
+    )
+}
+
+export default Contato
